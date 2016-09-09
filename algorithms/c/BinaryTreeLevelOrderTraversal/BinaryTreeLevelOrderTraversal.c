735a03fcce07fc892972b6860186fad31e2e3611
xsu11 <xsu11@hawk.iit.edu>
2016-09-09 11:15:19 -0500

    Solution for Binary Tree Level Order Tracersal.

---
+++ algorithms/c/BinaryTreeLevelOrderTraversal/BinaryTreeLevelOrderTraversal.c
@@ -0,0 +1,196 @@
+/*
+ * BinaryTreeLevelOrderTraversal.c
+ *
+ *  Created on: Sep 08, 2016
+ *      Author: xinsu
+ */
+
+/*
+ Given a binary tree, return the level order traversal of its nodes' values.
+ (ie, from left to right, level by level).
+
+ For example:
+ Given binary tree [3,9,20,null,null,15,7],
+ 3
+ / \
+ 9  20
+ /  \
+   15  7
+ return its level order traversal as:
+ [
+ [3],
+ [9,20],
+ [15,7]
+ ]
+ */
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+/**
+ * Return an array of arrays of size *returnSize.
+ * The sizes of the arrays are returned as *columnSizes array.
+ * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
+ */
+
+#include <stdio.h>
+#include <stdbool.h>
+
+// Add this struct difinition to scilent the show or error.
+struct TreeNode {
+	int val;
+	struct TreeNode *left;
+	struct TreeNode *right;
+};
+
+/*** Queue structure definition ***/
+struct QueueNode {
+	int info;
+	struct TreeNode *data;
+	struct QueueNode *next;
+};
+
+struct QueueTreeNode {
+	int size;
+	struct QueueNode *front;
+	struct QueueNode *back;
+};
+/*** Queue structure definition end ***/
+
+/*** Queue operations definition ***/
+struct QueueTreeNode *create() {
+	struct QueueTreeNode *queue = (struct QueueTreeNode *) calloc(1,
+			sizeof(struct QueueTreeNode));
+	return queue;
+}
+
+void destroy(struct QueueTreeNode *queue) {
+	if (queue != NULL) {
+		if (queue->front != NULL) {
+			struct QueueNode *curNode = queue->front;
+			while (queue->front != NULL) {
+				queue->front = queue->front->next;
+				free(curNode);
+				curNode = queue->front;
+			}
+		}
+		free(queue);
+	}
+}
+
+struct QueueNode *head(struct QueueTreeNode *queue) {
+	if (queue == NULL || queue->size == 0) {
+		return NULL;
+	}
+
+	return queue->front;
+}
+
+bool inqueue(struct QueueTreeNode *queue, struct TreeNode *data, int info) {
+	if (queue == NULL) {
+		return false;
+	}
+
+	// alloc memory for new node
+	struct QueueNode *node = (struct QueueNode *) calloc(1,
+			sizeof(struct QueueNode));
+	node->data = data;
+	node->info = info;
+
+	if (queue->size == 0) {
+		queue->front = node;
+	} else {
+		queue->back->next = node;
+	}
+	queue->back = node;
+	queue->size++;
+
+	return true;
+}
+
+struct TreeNode *dequeue(struct QueueTreeNode *queue, int *info) {
+	if (queue == NULL || queue->size == 0) {
+		return NULL;
+	}
+
+	struct QueueNode *node = queue->front;
+	struct TreeNode *data = node->data;
+	if (info != NULL) {
+		*info = node->info;
+	}
+
+	if (queue->size == 1) {
+		queue->front = NULL;
+		queue->back = NULL;
+	} else {
+		queue->front = queue->front->next;
+	}
+	queue->size--;
+	free(node);
+	return data;
+}
+/*** Queue operations definition end ***/
+
+int** levelOrder(struct TreeNode* root, int** columnSizes, int* returnSize) {
+	if (root == NULL) {
+		*columnSizes = NULL;
+		returnSize = NULL;
+		return NULL;
+	}
+
+	// First use queue1 do BFS, but put all the dequeued elements into queue2.
+	struct QueueTreeNode *queue1 = create();
+	struct QueueTreeNode *queue2 = create();
+	struct TreeNode *curTreeNode = NULL;
+	int level = 0;
+	inqueue(queue1, root, level);
+
+	while ((curTreeNode = dequeue(queue1, &level)) != NULL) {
+		inqueue(queue2, curTreeNode, level); // put all elements into queue2
+		if (curTreeNode->left != NULL) {
+			inqueue(queue1, curTreeNode->left, level + 1);
+		}
+		if (curTreeNode->right != NULL) {
+			inqueue(queue1, curTreeNode->right, level + 1);
+		}
+	}
+
+	*returnSize = level + 1;
+
+	// alloc memory for columnSizes based on returnSize
+	*columnSizes = (int *) calloc(*returnSize, sizeof(int));
+
+	// Then traverse the queue2 and store the columnSizes
+	struct QueueNode *curQueueNode = queue2->front;
+	while (curQueueNode != NULL) {
+		columnSizes[0][curQueueNode->info]++;
+		curQueueNode = curQueueNode->next;
+	}
+
+	// Then use columnSizes to alloc memory for result,
+	// but after allocation for each column, clear column size
+	int **result = (int **) calloc(*returnSize, sizeof(int *));
+	int i = 0;
+	for (i = 0; i < *returnSize; i++) {
+		result[i] = (int *) calloc(columnSizes[0][i], sizeof(int));
+		columnSizes[0][i] = 0;
+	}
+
+	// Traverse queue2 again and set result
+	curQueueNode = queue2->front;
+	while (curQueueNode != NULL) {
+		result[curQueueNode->info][columnSizes[0][curQueueNode->info]] =
+				curQueueNode->data->val;
+		columnSizes[0][curQueueNode->info]++;
+		curQueueNode = curQueueNode->next;
+	}
+
+	destroy(queue1);
+	destroy(queue2);
+	return result;
+}
