1eea96653d36e4bbd8bed1e505e32d1d1c8cb950
xsu11 <xsu11@hawk.iit.edu>
2016-09-10 16:26:39 -0500

    Solutions for Pascal's Triangle.

---
+++ algorithms/c/PascalTriangle/PascalTriangle1.c
@@ -0,0 +1,74 @@
+/*
+ * PascalTriangle.c
+ *
+ *  Created on: Sep 09, 2016
+ *      Author: xinsu
+ *
+ * Recursion
+ */
+
+/*
+ Given numRows, generate the first numRows of Pascal's triangle.
+
+ For example, given numRows = 5,
+ Return
+
+ [
+ [1],
+ [1,1],
+ [1,2,1],
+ [1,3,3,1],
+ [1,4,6,4,1]
+ ]
+ */
+
+/**
+ * Return an array of arrays.
+ * The sizes of the arrays are returned as *columnSizes array.
+ * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
+ */
+
+#include <stdio.h>
+
+void generateLine(int numRows, int *columnSizes, int **result) {
+	int idx = numRows - 1;
+
+	// alloc memory for the the lineNum-th line of result
+	result[idx] = (int *) calloc(columnSizes[idx], sizeof(int));
+
+	// base case
+	if (numRows == 1) {
+		result[0][0] = 1;
+		return;
+	}
+
+	// do tail recursion
+	generateLine(numRows - 1, columnSizes, result);
+
+	// create the line
+	int i = 0;
+	result[idx][i] = 1;
+	for (i = 1; i < columnSizes[idx] - 1; i++) {
+		result[idx][i] = result[idx - 1][i - 1] + result[idx - 1][i];
+	}
+	result[idx][i] = 1;
+}
+
+int** generate(int numRows, int** columnSizes) {
+	if (numRows <= 0) {
+		*columnSizes = NULL;
+		return NULL;
+	}
+
+	// Alloc memory for result. Alloc memory and set value for columnSizes.
+	int **result = (int **) calloc(numRows, sizeof(int *));
+	*columnSizes = (int *) calloc(numRows, sizeof(int));
+	int i = 0;
+	for (i = 0; i < numRows; i++) {
+		columnSizes[0][i] = i + 1;
+	}
+
+	// Call the tail-recursive function to finish the work.
+	generateLine(numRows, *columnSizes, result);
+	return result;
+}
---
+++ algorithms/c/PascalTriangle/PascalTriangle2.c
@@ -0,0 +1,62 @@
+/*
+ * PascalTriangle.c
+ *
+ *  Created on: Sep 10, 2016
+ *      Author: xinsu
+ *
+ * Iteration
+ */
+
+/*
+ Given numRows, generate the first numRows of Pascal's triangle.
+
+ For example, given numRows = 5,
+ Return
+
+ [
+ [1],
+ [1,1],
+ [1,2,1],
+ [1,3,3,1],
+ [1,4,6,4,1]
+ ]
+ */
+
+/**
+ * Return an array of arrays.
+ * The sizes of the arrays are returned as *columnSizes array.
+ * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
+ */
+
+#include <stdio.h>
+
+int** generate(int numRows, int** columnSizes) {
+	if (numRows <= 0) {
+		*columnSizes = NULL;
+		return NULL;
+	}
+
+	// alloc memory and set initial value for result and columnSizes
+	int **result = (int **) calloc(numRows, sizeof(int *));
+	result[0] = (int *) calloc(1, sizeof(int));
+	result[0][0] = 1;
+
+	*columnSizes = (int *) calloc(numRows, sizeof(int));
+	int i = 0;
+	for (i = 0; i < numRows; i++) {
+		columnSizes[0][i] = i + 1;
+	}
+
+	// create Pascal Triangle line by line
+	for (i = 1; i < numRows; i++) {
+		result[i] = (int *) calloc(columnSizes[0][i], sizeof(int));
+		result[i][0] = 1;
+		int j = 1;
+		for (j = 1; j < columnSizes[0][i] - 1; j++) {
+			result[i][j] = result[i - 1][j - 1] + result[i - 1][j];
+		}
+		result[i][j] = 1;
+	}
+
+	return result;
+}
