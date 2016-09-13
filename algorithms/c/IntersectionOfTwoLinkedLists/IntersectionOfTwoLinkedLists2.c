591b68bd83ae659deb0dd0586123f1372b0332b6
xsu11 <xsu11@hawk.iit.edu>
2016-09-13 10:02:35 -0500

    Add one more solution for Intersection of Two Linked Lists.

---
+++ algorithms/c/IntersectionOfTwoLinkedLists/IntersectionOfTwoLinkedLists2.c
@@ -0,0 +1,81 @@
+/*
+ * IntersectionOfTwoLinkedLists.c
+ *
+ *  Created on: Sep 12, 2016
+ *      Author: xinsu
+ */
+
+/*
+ * Write a program to find the node at which the intersection of two singly linked lists begins.
+ *
+ *
+ * For example, the following two linked lists:
+ *
+ * A:          a1 → a2
+ * 					  ↘
+ * 						c1 → c2 → c3
+ * 					  ↗
+ * B:     b1 → b2 → b3
+ * begin to intersect at node c1.
+ *
+ *
+ * Notes:
+ *
+ * If the two linked lists have no intersection at all, return null.
+ * The linked lists must retain their original structure after the function returns.
+ * You may assume there are no cycles anywhere in the entire linked structure.
+ * Your code should preferably run in O(n) time and use only O(1) memory.
+ * Credits:
+ * Special thanks to @stellari for adding this problem and creating all test cases.
+ */
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+#include <stdio.h>
+
+// Add this structure definition to silence the show of error.
+struct ListNode {
+	int val;
+	struct ListNode *next;
+};
+
+int getListLength(struct ListNode *head) {
+	int length = 0;
+	while (head != NULL) {
+		length++;
+		head = head->next;
+	}
+	return length;
+}
+
+struct ListNode *getIntersectionNode(struct ListNode *headA,
+		struct ListNode *headB) {
+	int lengthA = getListLength(headA);
+	int lengthB = getListLength(headB);
+
+	struct ListNode *curA = headA;
+	struct ListNode *curB = headB;
+	int i = 0;
+	if (lengthA > lengthB) {
+		for (i = 0; i < lengthA - lengthB; i++) {
+			curA = curA->next;
+		}
+	} else {
+		for (i = 0; i < lengthB - lengthA; i++) {
+			curB = curB->next;
+		}
+	}
+
+	while (curA != curB) {
+		curA = curA->next;
+		curB = curB->next;
+	}
+
+	return curA;
+}
