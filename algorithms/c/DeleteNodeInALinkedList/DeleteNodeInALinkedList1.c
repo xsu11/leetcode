38f09c25aac13dd0476bb7fc8203ef9c8bd033fe
xsu11 <xsu11@hawk.iit.edu>
2016-09-22 18:01:55 -0500

    Solutions for Delete Node in a Linked List.

---
+++ algorithms/c/DeleteNodeInALinkedList/DeleteNodeInALinkedList1.c
@@ -0,0 +1,40 @@
+/*
+ * DeleteNodeInALinkedList1.c
+ *
+ *  Created on: Sep 22, 2016
+ *      Author: xinsu
+ *
+ * Not an actually delete, just copy all the data from next to this node and then delete next node.
+ */
+
+/*
+ Write a function to delete a node (except the tail) in a singly linked list, given only access to that node.
+
+ Supposed the linked list is 1 -> 2 -> 3 -> 4 and you are given the third node with value 3,
+ the linked list should become 1 -> 2 -> 4 after calling your function.
+ */
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+#include <stdio.h>
+
+// Add this structure definition to silence the show of error.
+struct ListNode {
+	int val;
+	struct ListNode *next;
+};
+
+void deleteNode(struct ListNode* node) {
+	struct ListNode *nextNode = node->next;
+
+	node->val = node->next->val;
+	node->next = node->next->next;
+
+	free(nextNode);
+}
